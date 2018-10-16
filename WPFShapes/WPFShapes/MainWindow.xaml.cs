8edd6287d7ad5a943e49a5b1c739e292bdc592f2
Volodymyr Lysyshyn <lysyshynvova@gmail.com>
2018-10-16 18:31:17 +0100

    New initial commit

--- WPFShapes/WPFShapes/MainWindow.xaml.cs
+++ WPFShapes/WPFShapes/MainWindow.xaml.cs
@@ -13,18 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPFShapes
+namespace wpfShapes
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-           
-            
         }
     }
 }
--- WPFShapes/WPFShapes/Properties/Settings.Designer.cs
+++ WPFShapes/WPFShapes/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WPFShapes.Properties
+namespace wpfShapes.Properties
 {
 
 
